2d8e4590079c2258e16217ea53abae99662caa44
ideepler <ideepler@gmail.com>
2025-10-31 14:49:38 +0800

    增加加密解密函数

---
+++ src/Utils/CryptUtil.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ideepler\HyperfCore\Utils;
+
+/**
+ * 加密相关方法
+ */
+class CryptUtil
+{
+    /**
+     * 数据加密
+     * @param string $originStr 源字符串
+     * @param string $key 秘钥
+     * @return string chars 加密后的字符串
+     */
+    public function encrypt(string $originStr, string $key): string
+    {
+        $iv = openssl_random_pseudo_bytes(16);
+        $encrypted = openssl_encrypt($originStr, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+        return base64_encode($iv . $encrypted);
+    }
+
+    /**
+     * 数据解密
+     * @param string $encryptStr 源字符串
+     * @param string $key 秘钥
+     * @return string chars 加密后的字符串
+     */
+    public function decrypt(string $encryptStr, string $key): string
+    {
+        $decodedStr = base64_decode($encryptStr);
+        $iv = substr($decodedStr, 0, 16);
+        $decodedStrTrimIv = substr($decodedStr, 16);
+        return openssl_decrypt($decodedStrTrimIv, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+    }
+}
